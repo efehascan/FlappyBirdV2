e56b3b27b9775d92896f2f1aab9d99441ddbf292
Efe Hascan <82836263+efehascan@users.noreply.github.com>
2025-08-13 17:17:48 +0300

    Player

---
+++ Assets/Flappybird/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Flappybird.Scripts.Player
+{
+    
+    [RequireComponent((typeof(Rigidbody2D)))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float verticalSpeed;
+        [SerializeField] private float rotationSpeed;
+        
+        private Rigidbody2D rb2d;
+
+        private void Awake()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            VerticalMovement();
+        }
+
+
+        private void VerticalMovement()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            { 
+                rb2d.velocity = Vector2.up * verticalSpeed;
+            }
+            
+            transform.rotation = Quaternion.Euler(0, 0, rb2d.velocity.y * rotationSpeed);
+        }
+
+
+    }
+}
