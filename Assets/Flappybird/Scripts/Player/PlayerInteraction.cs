973a4e7ad54593ab5fbbe46372ffbdd0974696f9
Efe Hascan <82836263+efehascan@users.noreply.github.com>
2025-08-13 04:32:33 +0300

    PlayerInteraction

---
+++ Assets/Flappybird/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Flappybird.Scripts.ObserverPattern;
+using UnityEngine;
+
+namespace Flappybird.Scripts.Player
+{
+    public class PlayerInteraction : Subject
+    {
+        private const string Coin = "Coin";
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag(Coin))
+            {
+                NotifyObservers();
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
