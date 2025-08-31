9e6743d5fb2d120b5ae69c21e8eb1dfec1d9cec2
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-08-31 15:22:05 +0300

    SkinDatabase

---
+++ Assets/Flappybird/Scripts/UI/Shop/SkinDatabase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Flappybird.Scripts.UI.Shop
+{
+    [CreateAssetMenu(fileName = "SkinDatabase", menuName = "Game/Skin Database")]
+    public class SkinDatabase : ScriptableObject
+    {
+        [SerializeField] private SkinItem[] items;
+        public SkinItem[] Items => items;
+
+        public SkinItem Find(string id)
+        {
+            foreach (var s in items) if (s.Id == id) return s;
+            return null;
+        }
+    }
+}
