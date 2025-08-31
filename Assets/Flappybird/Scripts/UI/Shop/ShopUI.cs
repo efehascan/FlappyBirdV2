c2208e75b23568a8476ed0d5ffa53f4ec5365429
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-08-31 15:51:56 +0300

    ShopUI

---
+++ Assets/Flappybird/Scripts/UI/Shop/ShopUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Flappybird.Scripts.UI.Shop
+{
+    public class ShopUI : MonoBehaviour
+    {
+        [SerializeField] private SkinDatabase database;
+        [SerializeField] private Transform contentParent;
+        [SerializeField] private SkinButton skinButtonPrefab;
+        
+        private void OnEnable()
+        {
+            Rebuild();
+            SkinManager.Instance.onPurchased += _ => Rebuild();
+            SkinManager.Instance.onEquippedChanged += _ => Rebuild();
+        }
+
+        private void OnDisable()
+        {
+            if (SkinManager.Instance == null) return;
+            SkinManager.Instance.onPurchased -= _ => Rebuild();
+            SkinManager.Instance.onEquippedChanged -= _ => Rebuild();
+        }
+
+        private void Rebuild()
+        {
+            foreach (Transform t in contentParent) Destroy(t.gameObject);
+            foreach (var item in database.Items)
+                Instantiate(skinButtonPrefab, contentParent).Bind(item);
+        }
+        
+    }
+}
