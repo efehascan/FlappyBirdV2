079125e7de1ed91ab01da2587f9aeefc72c1049d
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-08-31 15:19:33 +0300

    SkinItem

---
+++ Assets/Flappybird/Scripts/UI/Shop/SkinItem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Flappybird.Scripts.UI.Shop
+{
+    [CreateAssetMenu(fileName = "SkinItem", menuName = "Game/Skin Item")]
+    public class SkinItem : ScriptableObject
+    {
+        [SerializeField] private string id;
+        [SerializeField] private string displayName;
+        [SerializeField] private int price;
+        [Header("Visual")]
+        [SerializeField] private Sprite sprite;
+        [SerializeField] private RuntimeAnimatorController animatorOverride;
+
+        public string Id => id;
+        public string DisplayName => displayName;
+        public int Price => price;
+        public Sprite Sprite => sprite;
+        public RuntimeAnimatorController AnimatorOverride => animatorOverride;
+        
+    }
+} 
